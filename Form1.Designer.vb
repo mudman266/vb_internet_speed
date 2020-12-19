f2f817fb207275ff0bb75bf49e57784c2c4400ec
Josh Williams <josh@jwillcreations.com>
2020-12-19 16:49:59 -0500

    Adding existing files

---
+++ Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.picBackground = New System.Windows.Forms.PictureBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.btnEnterSpeed = New System.Windows.Forms.Button()
+        Me.lblAvgSpeed = New System.Windows.Forms.Label()
+        Me.lstSpeeds = New System.Windows.Forms.ListBox()
+        CType(Me.picBackground, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picBackground
+        '
+        Me.picBackground.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picBackground.Image = CType(resources.GetObject("picBackground.Image"), System.Drawing.Image)
+        Me.picBackground.Location = New System.Drawing.Point(0, 0)
+        Me.picBackground.Name = "picBackground"
+        Me.picBackground.Size = New System.Drawing.Size(892, 454)
+        Me.picBackground.TabIndex = 0
+        Me.picBackground.TabStop = False
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.BackColor = System.Drawing.Color.White
+        Me.lblTitle.Font = New System.Drawing.Font("Bookman Old Style", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(275, 19)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(429, 35)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "Internet Speed Test Survey"
+        '
+        'btnEnterSpeed
+        '
+        Me.btnEnterSpeed.Font = New System.Drawing.Font("Bookman Old Style", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEnterSpeed.Location = New System.Drawing.Point(344, 93)
+        Me.btnEnterSpeed.Name = "btnEnterSpeed"
+        Me.btnEnterSpeed.Size = New System.Drawing.Size(268, 64)
+        Me.btnEnterSpeed.TabIndex = 2
+        Me.btnEnterSpeed.Text = "Enter Internet Speed"
+        Me.btnEnterSpeed.UseVisualStyleBackColor = True
+        '
+        'lblAvgSpeed
+        '
+        Me.lblAvgSpeed.AutoSize = True
+        Me.lblAvgSpeed.BackColor = System.Drawing.Color.White
+        Me.lblAvgSpeed.Font = New System.Drawing.Font("Bookman Old Style", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAvgSpeed.Location = New System.Drawing.Point(423, 417)
+        Me.lblAvgSpeed.Name = "lblAvgSpeed"
+        Me.lblAvgSpeed.Size = New System.Drawing.Size(262, 24)
+        Me.lblAvgSpeed.TabIndex = 3
+        Me.lblAvgSpeed.Text = "Average Internet Speed: "
+        '
+        'lstSpeeds
+        '
+        Me.lstSpeeds.FormattingEnabled = True
+        Me.lstSpeeds.Location = New System.Drawing.Point(658, 93)
+        Me.lstSpeeds.Name = "lstSpeeds"
+        Me.lstSpeeds.Size = New System.Drawing.Size(92, 95)
+        Me.lstSpeeds.TabIndex = 4
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(893, 450)
+        Me.Controls.Add(Me.lstSpeeds)
+        Me.Controls.Add(Me.lblAvgSpeed)
+        Me.Controls.Add(Me.btnEnterSpeed)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.picBackground)
+        Me.Name = "frmMain"
+        Me.Text = "Internet Speed Test Survey"
+        CType(Me.picBackground, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picBackground As PictureBox
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents btnEnterSpeed As Button
+    Friend WithEvents lblAvgSpeed As Label
+    Friend WithEvents lstSpeeds As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class frmMain
+    Private Sub btnEnterSpeed_Click(sender As Object, e As EventArgs) Handles btnEnterSpeed.Click
+        ' Clear any entries that may exist
+        lstSpeeds.Items.Clear()
+        lblAvgSpeed.Text = "Average Internet Speed: "
+
+        Dim strEnteredSpeed As String
+        Dim intNumberEntries As Integer
+
+        ' Try to loop 10 times and get an entry from the user
+        For intNumberEntries = 1 To 10
+            strEnteredSpeed = InputBox("Enter internet speed in Mbps.", "Internet Speed")
+
+            ' If the input contains something, validate it and add to the collection
+            If strEnteredSpeed <> "" Then
+
+                ' Handle non numeric entry
+                While Not IsNumeric(strEnteredSpeed)
+                    MsgBox("Your input was non numeric. Try again.")
+                    strEnteredSpeed = InputBox("Enter internet speed in Mbps.", "Internet Speed")
+                End While
+
+                ' Handle negative value entry
+                While strEnteredSpeed < 0
+                    MsgBox("Your input was a negative value. Try again.")
+                    strEnteredSpeed = InputBox("Enter internet speed in Mbps.", "Internet Speed")
+                    ' Handle non numeric entry
+                    While Not IsNumeric(strEnteredSpeed)
+                        MsgBox("Your input was non numeric. Try again.")
+                        strEnteredSpeed = InputBox("Enter internet speed in Mbps.", "Internet Speed")
+                    End While
+                End While
+
+                ' Valid entry, add to collection
+                lstSpeeds.Items.Add(strEnteredSpeed)
+            Else
+
+                ' Blank entry or cancel pressed
+                ' If no entries exist, warn user and exit sub
+                If lstSpeeds.Items.Count < 1 Then
+                    MsgBox("You did not enter any values.")
+                    Exit Sub
+                End If
+
+                ' Entries exist, exit for loop.
+                Exit For
+            End If
+        Next
+
+        ' Got all entries, find the average
+        Dim i As Integer
+        Dim decSum As Decimal
+        Dim decAvg As Decimal
+        For i = 0 To (lstSpeeds.Items.Count - 1)
+            decSum += lstSpeeds.Items(i)
+        Next
+        decAvg = decSum / lstSpeeds.Items.Count()
+
+        ' Update the label
+        lblAvgSpeed.Text = "Average Internet Speed: " & decAvg.ToString("F2") & " Mbps"
+    End Sub
+End Class
